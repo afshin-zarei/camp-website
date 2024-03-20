9dbc7bdeddd2a42fc02a83f79aeafe1a527f40c5
Afshin Zarei <afshin.zarei2000@gmail.com>
2024-03-20 12:02:10 +0330

    Added mobile navbar.

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,10 @@
 const navBtn = document.querySelector(".nav__btn");
+const navMenu = document.querySelector(".nav-menu");
 
 navBtn.addEventListener("click", function (){
         navBtn.classList.toggle("nav__btn--open");
+        navMenu.classList.toggle("nav-menu--open")
+
 });
+
 
